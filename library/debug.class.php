54e12bdfb9ff687b8bffcdec88f1359d4724e87c
Marc Dannemann <marc.dannemann@gmx.net>
2012-01-18 21:39:01 +0100

    ! Debugausgabe weiter optimiert, str_pad statt str_repeat in der Ueberschrift

--- library/debug.class.php
+++ library/debug.class.php
@@ -14,7 +14,7 @@ class Debug {
   
   public static function generateOutput() {
     $output  = '<div id="debug">'."\n";
-    $output .= str_repeat('=', 46).' DEBUG '.str_repeat('=', 47)."<br />\n";
+    $output .= str_pad(' DEBUG ', 100, '=', STR_PAD_BOTH)."<br />\n";
     foreach(self::$_messages as $msg)
       $output .= $msg;
     $output .= str_repeat('=', 100)."<br />\n</div>\n";
